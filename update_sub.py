0741341c4b44572f79e0b0731f2dac80a6a0926b
ningwang-arch <ningwang778@gmail.com>
2021-03-27 09:57:41 +0800

    new method in update_sub.py

--- update_sub.py
+++ update_sub.py
@@ -1,28 +1,75 @@
 import re
 import base64
 import urllib3
+import os
+from vm2obs import convert
+
+
+sub_path = 'subscribe.txt'
+
+
+def force_input():
+    subscribe = ''
+    while subscribe == '':
+        subscribe = input()
+    return subscribe
 
 
 # 从subscribe.txt中读取链接
 def update_from_txt():
-    with open("subscribe.txt", 'r') as f:
+    if not os.path.exists(sub_path):
+        url = force_input()
+        update_from_url(url)
+        return
+    with open(sub_path, 'r') as f:
         subscribe = f.readline()
         if subscribe == "":
-            # 转移到强制输入订阅链接
+            url = force_input()
+            update_from_url(url)
             return
         if re.match(r'^https?:/{2}\w.+$', subscribe):
             http = urllib3.PoolManager()
-            str_b64 = http.request('GET', subscribe).data.decode()
-
+            response = http.request('GET', subscribe)
+            if response.status == 200:
+                str_b64 = response.data.decode()
+            else:
+                print('Invalid subscription link')
+                return
         else:
             str_b64 = subscribe
     blen = len(str_b64)
     if blen % 4 > 0:
         str_b64 += "=" * (4 - blen % 4)
     str_links = base64.b64decode(str_b64).decode()
-    v_list = str_links.split('\r\n')[:-2]
-    print(v_list)
+    v_list = str_links.split('\r\n')
+    for item in v_list:
+        if item == "":
+            continue
+        else:
+            convert(item)
+
+
+def update_from_url(url):
+    with open(sub_path, 'r') as f:
+        f.write(url)
+    http = urllib3.PoolManager()
+    response = http.request('GET', url)
+    if response.status == 200:
+        str_b64 = response.data.decode()
+    else:
+        print('Invalid subscription link')
+        return
+    blen = len(str_b64)
+    if blen % 4 > 0:
+        str_b64 += "=" * (4 - blen % 4)
+    str_links = base64.b64decode(str_b64).decode()
+    v_list = str_links.split('\r\n')
+    for item in v_list:
+        if item == "":
+            continue
+        else:
+            convert(item)
 
 
 if __name__ == '__main__':
-    update_from_txt()
+    print(force_input())
